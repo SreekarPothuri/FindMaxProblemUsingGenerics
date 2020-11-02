cbcc8026e102cb2b6760bc943e2f342170fdedd8
Your Name <you@example.com>
2020-11-02 20:58:42 +0530

    Added: Integer comparision to find maxInt

--- src/main/java/findMaximumProblem/FindMaximum.java
+++ src/main/java/findMaximumProblem/FindMaximum.java
@@ -1,7 +1,23 @@
 package findMaximumProblem;
 
 public class FindMaximum {
+
+	public int compare(Integer o1, Integer o2) {
+		return o1.compareTo(o2);
+	}
+
+	public static int maxInt(Integer a, Integer b, Integer c) {
+		int max = a;
+
+		if (b.compareTo(a) > 0)
+			max = b;
+		if (c.compareTo(b) > 0)
+			max = c;
+		return max;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("*****WELCOME TO FIND MAXIMUM PROBLEM*****");
+		System.out.println("Max int: " + maxInt(777, 8888, 200000));
 	}
 }
