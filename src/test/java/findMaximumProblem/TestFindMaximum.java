da25a88eef93d9393741d9e9d236aced52b47336
Your Name <you@example.com>
2020-11-02 21:47:04 +0530

    Added: Test cases for maxString method

--- src/test/java/findMaximumProblem/TestFindMaximum.java
+++ src/test/java/findMaximumProblem/TestFindMaximum.java
@@ -9,7 +9,7 @@ import findMaximumProblem.FindMaximum;
 public class TestFindMaximum {
 
 	static FindMaximum maxi;
-	
+
 	@BeforeClass
 	public static void findMaximumObj() {
 		maxi = new FindMaximum();
@@ -19,36 +19,54 @@ public class TestFindMaximum {
 	@Test
 	public void testMaximumAtFirstPositionForInteger_testAssertionHappy() {
 		int result = maxi.maxInt(100, 20, 3);
-		Assert.assertEquals(100,result);
+		Assert.assertEquals(100, result);
 	}
-	
+
 	@Test
 	public void testMaximumAtSecondPositionForInteger_testAssertionHappy() {
 		int result = maxi.maxInt(10, 20, 3);
-		Assert.assertEquals(20,result);
+		Assert.assertEquals(20, result);
 	}
-	
+
 	@Test
 	public void testMaximumAtThirdPositionForInteger_testAssertionHappy() {
 		int result = maxi.maxInt(1, 22, 356);
-		Assert.assertEquals(356,result);
+		Assert.assertEquals(356, result);
 	}
-	
+
 	@Test
 	public void testMaximumAtFirstPositionForFloat_testAssertionHappy() {
 		float result = maxi.maxFloat(100.25f, 22.256f, 35.6f);
 		Assert.assertEquals(result, 100.0, 0.25);
 	}
-	
+
 	@Test
 	public void testMaximumAtSecondPositionForFloat_testAssertionHappy() {
 		float result = maxi.maxFloat(10.0f, 222.256f, 35.6f);
 		Assert.assertEquals(result, 222.006, 0.25);
 	}
-	
+
 	@Test
 	public void testMaximumAtThirdPositionForFloat_testAssertionHappy() {
 		float result = maxi.maxFloat(10.0f, 22.2f, 35.6f);
 		Assert.assertEquals(result, 35.00, 0.6);
+	}
+
+	@Test
+	public void testMaximumAtFirstPositionForString_testAssertionHappy() {
+		String result = maxi.maxString("Srinu", "Latha", "Sree");
+		Assert.assertEquals("Srinu", result);
+	}
+
+	@Test
+	public void testMaximumAtSecondPositionForString_testAssertionHappy() {
+		String result = maxi.maxString("Latha", "Srinu", "Sree");
+		Assert.assertEquals("Srinu", result);
+	}
+
+	@Test
+	public void testMaximumAtThirdPositionForString_testAssertionHappy() {
+		String result = maxi.maxString("Sree", "Latha", "Srinu");
+		Assert.assertEquals("Srinu", result);
 	}
 }
