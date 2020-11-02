4597f6bf32fc0f999aca477479c8b59e1427f8a4
Your Name <you@example.com>
2020-11-02 23:57:29 +0530

    Refactored: With Generic Class

--- src/main/java/findMaximumProblem/FindMaximum.java
+++ src/main/java/findMaximumProblem/FindMaximum.java
@@ -1,32 +1,29 @@
 package findMaximumProblem;
 
-public class FindMaximum {
+public class FindMaximum <T extends Comparable<T>> {
 
-	public int compare(Integer o1, Integer o2) {
-		return o1.compareTo(o2);
+	T a;
+	T b;
+	T c;
+	
+	public FindMaximum(T a, T b, T c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
 	}
 
-	public float compare(Float o1, Float o2) {
-		return o1.compareTo(o2);
-	}
-
-	public int compare(String o1, String o2) {
-		return o1.compareTo(o2);
-	}
-
-	public static <T extends Comparable<T>> T max(T a, T b, T c) {
-		T max = a;
-		if (b.compareTo(a) > 0)
-			max = b;
-		if (c.compareTo(b) > 0)
-			max = c;
+	public <T extends Comparable<T>> T testMaximum() {
+		T max = (T) a;
+		if (b.compareTo(a) > 0) {
+			max = (T) b;
+		}
+		if (c.compareTo(b) > 0) {
+			max = (T) c;
+		}
 		return max;
 	}
 
 	public static void main(String[] args) {
 		System.out.println("*****WELCOME TO FIND MAXIMUM PROBLEM*****");
-		System.out.println("Max int: " + max(777, 8888, 200000));
-		System.out.println("Max float: " + max(7.056f, 88.88f, 20.000f));
-		System.out.println("Max String: " + max("Apple", "Peach", "Banana"));
 	}
 }
