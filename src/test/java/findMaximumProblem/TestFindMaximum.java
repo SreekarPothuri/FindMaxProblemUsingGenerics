6384eac4d238751f9a70f16cddc5656287f17f5e
Your Name <you@example.com>
2020-11-03 08:38:56 +0530

    Refactored: Test cases with generic method

--- src/test/java/findMaximumProblem/TestFindMaximum.java
+++ src/test/java/findMaximumProblem/TestFindMaximum.java
@@ -18,37 +18,37 @@ public class TestFindMaximum {
 
 	@Test
 	public void testMaximumAtFirstPositionForInteger_testAssertionHappy() {
-		int result = maxi.max(100, 20, 3);
-		Assert.assertEquals(100, result);
+		Integer result = maxi.max(100, 20, 3);
+		Assert.assertEquals(Integer.valueOf(100), result);
 	}
 
 	@Test
 	public void testMaximumAtSecondPositionForInteger_testAssertionHappy() {
-		int result = maxi.max(10, 20, 3);
-		Assert.assertEquals(20, result);
+		Integer result = maxi.max(10, 20, 3);
+		Assert.assertEquals(Integer.valueOf(20), result);
 	}
 
 	@Test
 	public void testMaximumAtThirdPositionForInteger_testAssertionHappy() {
-		int result = maxi.max(1, 22, 356);
-		Assert.assertEquals(356, result);
+		Integer result = maxi.max(1, 22, 356);
+		Assert.assertEquals(Integer.valueOf(356), result);
 	}
 
 	@Test
 	public void testMaximumAtFirstPositionForFloat_testAssertionHappy() {
-		float result = maxi.max(100.08f, 20.25f, 3.56f);
-		Assert.assertEquals(result, 100.00, 0.08);
+		Float result = maxi.max(100.08f, 20.25f, 3.56f);
+		Assert.assertEquals(result, 100.08, 0.08);
 	}
 
 	@Test
 	public void testMaximumAtSecondPositionForFloat_testAssertionHappy() {
-		float result = maxi.max(10.0f, 222.256f, 35.6f);
+		Float result = maxi.max(10.0f, 222.256f, 35.6f);
 		Assert.assertEquals(result, 222.006, 0.25);
 	}
 
 	@Test
 	public void testMaximumAtThirdPositionForFloat_testAssertionHappy() {
-		float result = maxi.max(10.0f, 22.2f, 35.6f);
+		Float result = maxi.max(10.0f, 22.2f, 35.6f);
 		Assert.assertEquals(result, 35.00, 0.6);
 	}
 
