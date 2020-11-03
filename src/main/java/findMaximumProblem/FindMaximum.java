7a6fa7191c1e01a5b38d3e7fd3de6053c06bed73
Your Name <you@example.com>
2020-11-03 13:36:03 +0530

    Added: printValue method

--- src/main/java/findMaximumProblem/FindMaximum.java
+++ src/main/java/findMaximumProblem/FindMaximum.java
@@ -1,14 +1,19 @@
 package findMaximumProblem;
 
-public class FindMaximum <T extends Comparable<T>> {
+public class FindMaximum<T extends Comparable<T>> {
 
 	public static <T extends Comparable<T>> T testMaximum(T... var) {
 		T max = var[0];
-		for(T type: var) {
-			if(type.compareTo(max) > 0)
+		for (T type : var) {
+			if (type.compareTo(max) > 0)
 				max = type;
 		}
+		printValue(max);
 		return max;
+	}
+
+	public static <T> void printValue(T max) {
+		System.out.println(" Max: "+max);
 	}
 
 	public static void main(String[] args) {
