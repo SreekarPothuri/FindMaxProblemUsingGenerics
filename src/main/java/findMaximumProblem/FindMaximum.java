b7e8e000055ee3667d4f00590f9de400e5e723f9
Your Name <you@example.com>
2020-11-03 13:19:40 +0530

    Added: varArgs to enter multiple parameters

--- src/main/java/findMaximumProblem/FindMaximum.java
+++ src/main/java/findMaximumProblem/FindMaximum.java
@@ -2,27 +2,11 @@ package findMaximumProblem;
 
 public class FindMaximum <T extends Comparable<T>> {
 
-	T a;
-	T b;
-	T c;
-	
-	public FindMaximum(T a, T b, T c) {
-		this.a = a;
-		this.b = b;
-		this.c = c;
-	}
-	
-	public T testMaximum() {
-		return (T) FindMaximum.testMaximum(a, b, c);
-	}
-
-	public static <T extends Comparable<T>> T testMaximum(T a, T b, T c) {
-		T max = (T) a;
-		if (b.compareTo(max) > 0) {
-			max = (T) b;
-		}
-		if (c.compareTo(max) > 0) {
-			max = (T) c;
+	public static <T extends Comparable<T>> T testMaximum(T... var) {
+		T max = var[0];
+		for(T type: var) {
+			if(type.compareTo(max) > 0)
+				max = type;
 		}
 		return max;
 	}
